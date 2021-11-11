085d49d5cda4a6152d60d99303b33b95f0225b72
Jorge Vega <jorg363@gmail.com>
2021-11-11 13:38:25 -0600

    Fixing typos in my_dag.py

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator 
 from random import randint
 
-def _chose_best_model(ti):
+def _choose_best_model(ti):
     accuracies = ti.xcom_pull(task_ids=[
         'training_model_A',
         'training_model_B',
@@ -26,27 +26,29 @@ with DAG("my_dag", start_date=datetime(2021, 1, 1),
             python_callable=_training_model
         )
 
-                training_model_B = PythonOperator(
+        training_model_B = PythonOperator(
             task_id='training_model_B',
             python_callable=_training_model
         )
 
-                training_model_C = PythonOperator(
+        training_model_C = PythonOperator(
             task_id='training_model_C',
             python_callable=_training_model
         )
 
-        chose_best_model = BranchPythonOperator(
-            task_id='chose_best_model'
-            python_callable=_chose_best_model
+        choose_best_model = BranchPythonOperator(
+            task_id='choose_best_model',
+            python_callable=_choose_best_model
         )
 
         accurate = BashOperator(
             task_id='accurate',
-            bash_comand='echo "accurate"'
+            bash_command='echo "accurate"'
         )
 
         inaccurate = BashOperator(
             task_id='inaccurate',
-            bash_comand='echo "inaccurate"'
+            bash_command='echo "inaccurate"'
         )
+
+        [training_model_A, training_model_B, training_model_C] >> choose_best_model >> [accurate, inaccurate]
