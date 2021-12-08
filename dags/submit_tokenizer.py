8e224acd5385e5a57bdc91b4100eee5f997735cb
Jorge Vega <jorg363@gmail.com>
2021-12-08 13:55:59 -0600

    Rename file, added trigger rule to destroy cluster

--- dags/submit_tokenizer.py
+++ dags/submit_tokenizer.py
@@ -1,3 +1,6 @@
+# Airflow DAG to create a Dataproc cluster, submit a Pyspark Job from GCS and
+# destroy the cluster.
+
 from airflow import DAG
 from datetime import datetime
 from airflow.providers.google.cloud.operators.dataproc import (
@@ -6,20 +9,10 @@ from airflow.providers.google.cloud.operators.dataproc import (
     DataprocDeleteClusterOperator
 )
 
-# Default arguments 
-default_args = {
-    'owner': 'jorge.vega',
-    'depends_on_past': False,    
-    'start_date': datetime(2021, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False
-}
-
 CLUSTER_NAME = 'efimeral-cluster'
 REGION='us-east1'
 PROJECT_ID='capstone-jmv'
-PYSPARK_URI='gs://testing-bucket-wzln-0/tokenyzer.py'
-#BUCKET = 'wzlnde-staging-0'
+PYSPARK_URI='gs://wzlnde-code-0/pyspark/tokenizer.py'
 
 # Cluster definition
 CLUSTER_CONFIG = {
@@ -41,18 +34,27 @@ PYSPARK_JOB = {
     "pyspark_job": {"main_python_file_uri": PYSPARK_URI},
 }
 
+# Default arguments 
+default_args = {
+    'owner': 'jorge.vega',
+    'depends_on_past': False,    
+    'start_date': datetime(2021, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False
+}
+
 with DAG(
-    dag_id='create_submit_destroy_dataproc',
+    dag_id='submit_tokenizer',
     default_args=default_args,
     schedule_interval="@once",
-    catchup=False
+    catchup=False,
+    tags=['capstone']
     ) as dag:
 
     create_cluster = DataprocCreateClusterOperator(
         task_id="create_cluster",
         project_id=PROJECT_ID,
         cluster_config=CLUSTER_CONFIG,
-        #storage_bucket=BUCKET,
         region=REGION,
         cluster_name=CLUSTER_NAME,
     )
@@ -68,7 +70,8 @@ with DAG(
         task_id="delete_cluster",
         project_id=PROJECT_ID,
         cluster_name=CLUSTER_NAME,
-        region=REGION
+        region=REGION,
+        trigger_rule='all_done'
     )
 
     create_cluster >> pyspark_task >> delete_cluster
